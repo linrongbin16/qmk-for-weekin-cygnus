7cc07457a3c8e6a5ebe5216a7aa08d67c7067240
linrongbin16 <linrongbin16@outlook.com>
2024-11-03 14:14:33 +0800

    Disable prints

--- process.py
+++ process.py
@@ -11,14 +11,14 @@ with open('Cygnus-Keymap.yml', 'r') as src:
                 combos_section = True
             if combos_section and line.strip().startswith("- {"):
                 prefix_pos = line.find("- {")
-                print(f"prefix_pos:{prefix_pos}")
+                # print(f"prefix_pos:{prefix_pos}")
                 data = line[prefix_pos+2:]
-                print(f"data-1:{data}")
+                # print(f"data-1:{data}")
                 data = json.loads(data)
                 data['d'] = True
                 if isinstance(data['k'], dict) and isinstance(data['k']['s'], str):
                     data['k'].pop('s')
-                print(f"data-2:{data}")
+                # print(f"data-2:{data}")
                 dst_line = "".join([' ' for i in range(prefix_pos)]) + "- " + json.dumps(data, sort_keys=True) + "\n"
             dst.write(dst_line)
 
