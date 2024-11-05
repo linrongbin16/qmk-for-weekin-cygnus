f036182d0fb0ccb650c36f9488fc52e08897cdfd
linrongbin16 <linrongbin16@outlook.com>
2024-11-05 09:44:03 +0800

    Update drawer pipeline

--- process.py
+++ process.py
@@ -14,16 +14,16 @@ with open('Cygnus-Keymap.yml', 'r') as src:
                 index = str(int(line[prefix_pos+6:].strip()))
                 # print(f"prefix_pos:{prefix_pos}, suffix:{index}")
                 dst_line = "".join([' ' for i in range(prefix_pos)]) + "- \"User\\n" + index + "\"\n"
-            elif combos_section and line.strip().startswith("- {"):
-                prefix_pos = line.find("- {")
-                # print(f"prefix_pos:{prefix_pos}")
-                data = line[prefix_pos+2:]
-                # print(f"data-1:{data}")
-                data = json.loads(data)
-                # data['d'] = True
-                if isinstance(data['k'], dict) and isinstance(data['k']['s'], str):
-                    data['k'].pop('s')
-                # print(f"data-2:{data}")
-                dst_line = "".join([' ' for i in range(prefix_pos)]) + "- " + json.dumps(data, sort_keys=True) + "\n"
+            # elif combos_section and line.strip().startswith("- {"):
+            #     prefix_pos = line.find("- {")
+            #     # print(f"prefix_pos:{prefix_pos}")
+            #     data = line[prefix_pos+2:]
+            #     # print(f"data-1:{data}")
+            #     data = json.loads(data)
+            #     # data['d'] = True
+            #     if isinstance(data['k'], dict) and isinstance(data['k']['s'], str):
+            #         data['k'].pop('s')
+            #     # print(f"data-2:{data}")
+            #     dst_line = "".join([' ' for i in range(prefix_pos)]) + "- " + json.dumps(data, sort_keys=True) + "\n"
             dst.write(dst_line)
 
